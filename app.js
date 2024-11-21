cdc04d85ca503b31d39e9d8dacc2d485ff723d1d
Sourav <sourav123singhgmaii.com@gmail.com>
2024-11-21 20:51:20 +0530

    Custom Cursor Added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let menuIcon = document.getElementsByClassName('menu-icon');
 let sidebar = document.getElementsByClassName('sidebar');
 let hideMenu = document.getElementsByClassName('hide-menu');
+let cursor = document.querySelector(".cursor");
 
 menuIcon[0].addEventListener("click", () =>{
     sidebar[0].style.display = 'flex';
@@ -11,3 +12,9 @@ hideMenu[0].addEventListener("click", () =>{
     sidebar[0].style.display = 'none';
     console.log("done");
 })
+
+document.addEventListener("mousemove",(event)=>{
+    cursor.style.top = event.y + "px";
+    cursor.style.left = event.x + "px";
+})
+
